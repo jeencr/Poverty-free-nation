267e1cab782b5624f7deb864cae8909befaf9291
Jeevan C R <145118797+jeencr@users.noreply.github.com>
2025-09-26 22:47:13 +0530

    2nd commit

--- Poverty Free Nation Django/myapp/views.py
+++ Poverty Free Nation Django/myapp/views.py
@@ -28,13 +28,34 @@ def login_post(request):
         if user is not None:
             if user.groups.filter(name='admin').exists():
                 login(request, user)
-                return redirect('/myapp/view_adminhome', {'message': 'Login successful!'})
+                return redirect('/myapp/view_adminhome/', {'message': 'Login successful!'})
             elif user.groups.filter(name='company').exists():
                 login(request, user)
-                return redirect('/myapp/view_company_home',{'message': 'Login successful!'})
+                return redirect('/myapp/view_company_home/',{'message': 'Login successful!'})
             elif user.groups.filter(name='skill_center').exists():
                 login(request, user)
-                return redirect('/myapp/view_skill_center_home', {'message': 'Login successful!'})
+                return redirect('/myapp/view_skill_center_home/', {'message': 'Login successful!'})
 
         else:
             return render(request, 'login.html', {'error': 'Invalid username or password.'})
+
+def view_skill_center_register(request):
+    return render(request, 'SkillCenterRegisteration.html')
+
+def skill_center_register_post(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        address = request.POST['address']
+        username = request.POST['username']
+        password = request.POST['password']
+        photo = request.FILES['photo']
+        if User.objects.filter(username=username).exists():
+            return render(request, 'SkillCenterRegisteration.html', {'error': 'Username already exists.'})
+        user = User.objects.create_user(username=username, password=password)
+        skill_center = SkillCenterProfile(name=name,    address=address,  phone=phone,
+                                          approval_status='pending',  email=email,   photo=photo,
+                                          LOGIN_id=user.id )
+        skill_center.save()
+        return render(request, 'SkillCenterRegisteration.html', {'message': 'Registration successful!'})
